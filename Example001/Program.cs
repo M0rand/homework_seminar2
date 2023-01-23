d993e1b03412f77504df5b018795563a00dc8779
Vladimir Morar <morvladimir1981@gmail.com>
2023-01-24 01:48:33 +0300

    add Example 001

---
+++ Example001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberA = number /10;
+
+System.Console.WriteLine(numberA % 10);
