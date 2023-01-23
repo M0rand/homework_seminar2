dbeb20b92291f025323fb9172c1f6287b00173aa
Vladimir Morar <morvladimir1981@gmail.com>
2023-01-24 00:21:08 +0300

    add Example 3

---
+++ Example3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и выводит наибольшее число
+// 85 -> 8
+// 26 -> 6
+
+Random rand = new Random(); // Объявление объекта класса Random
+int randNumber = rand.Next(10, 100); // Генерация переменной от 10 до 99
+System.Console.WriteLine(randNumber);
+
+if(randNumber/10 > randNumber%10)
+{
+    System.Console.WriteLine(randNumber/10);
+}
+else
+{
+    System.Console.WriteLine(randNumber%10);
+}
