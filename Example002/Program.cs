ee015b73a85ed392556e7aec12faa53a304cca59
Vladimir Morar <morvladimir1981@gmail.com>
2023-01-24 02:14:17 +0300

    add Example 002

---
+++ Example002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberA = Convert.ToString(number);
+int size = numberA.ToString().Length;
+
+if(size < 3)
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    System.Console.WriteLine(numberA[2]);
+}
