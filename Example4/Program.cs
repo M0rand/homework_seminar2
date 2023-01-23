9a333398763905cab12a451262b6a4e3630dfb41
Vladimir Morar <morvladimir1981@gmail.com>
2023-01-24 00:37:30 +0300

    add Example 4

---
+++ Example4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу, которая выводит случайное трехзначное число и удаляет среднюю цифру
+// 486 -> 46
+
+Random rand = new Random(); // Объявление объекта класса Random
+int randNumber = rand.Next(100, 1000); // Генерация переменной от 100 до 999 
+System.Console.WriteLine(randNumber);
+int number1 = randNumber/100;
+int number2 = randNumber%10;
+System.Console.WriteLine(number1*10 + number2);
